d84d9ca07deb3f70d777705ae0999a566837791f
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-08-24 13:44:53 -0500

    FirstProgram project added

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("********************************************");
+            WriteLine("* PROGRAMA PARA CALCULAR ax^2 + bx + c = 0 *");
+            WriteLine("********************************************");
+            WriteLine("\nIntroduce los valores");
+
+            Write("a: ");
+            int a = Convert.ToInt32(ReadLine());
+        }
+    }
+}
