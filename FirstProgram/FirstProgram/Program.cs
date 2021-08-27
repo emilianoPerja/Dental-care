9cea3a3e15d49b78da4f0f0793bc85febf08d743
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-08-26 22:26:29 -0500

    Update Program.cs

--- FirstProgram/FirstProgram/Program.cs
+++ FirstProgram/FirstProgram/Program.cs
@@ -12,13 +12,71 @@ namespace FirstProgram
     {
         static void Main(string[] args)
         {
-            WriteLine("********************************************");
-            WriteLine("* PROGRAMA PARA CALCULAR ax^2 + bx + c = 0 *");
-            WriteLine("********************************************");
-            WriteLine("\nIntroduce los valores");
+            char option = 'n';
+            do
+            {
+                Clear();
+                WriteLine("********************************************");
+                WriteLine("* PROGRAMA PARA CALCULAR ax^2 + bx + c = 0 *");
+                WriteLine("********************************************");
+                WriteLine("\nIntroduce los valores");
 
-            Write("a: ");
-            int a = Convert.ToInt32(ReadLine());
+                Write("a: ");
+                int a = Convert.ToInt32(ReadLine());
+
+                Write("b: ");
+                int b = Convert.ToInt32(ReadLine());
+
+                Write("c: ");
+                int c = Convert.ToInt32(ReadLine());
+
+                if (a == 0 && b == 0)
+                {
+                    WriteLine("¡No es una ecuación válida!");
+                }
+                else if (a == 0) // && b != 0
+                {
+                    WriteLine("\n¡Es una ecuación lineal!");
+
+                    double x = -c / (double)b;
+                    WriteLine($"x = {x}");
+                }
+                else
+                {
+                    int D = b * b - 4 * a * c;
+                    if (D > 0)
+                    {
+                        WriteLine("\n¡Raíces reales y diferentes!");
+
+                        double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                        double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                        WriteLine($"x1 = {x1}");
+                        WriteLine($"x2 = {x2}");
+                    }
+                    else if (D == 0)
+                    {
+                        WriteLine("\n¡Raíces reales e iguales!");
+
+                        double x = -b / (2.0 * a);
+                        WriteLine($"x1 = x2 = {x}");
+                    }
+                    else
+                    {
+                        WriteLine("\n¡Raíces complejas conjugadas!");
+
+                        double real = -b / (2.0 * a);
+                        double imag = Math.Sqrt(-D) / (2 * a);
+
+                        WriteLine($"x1 = {real} + {imag} i");
+                        WriteLine($"x2 = {real} - {imag} i");
+                    }
+                }
+
+                Write("\n¿Deseas resolver otra ecuación? [s/n]: ");
+                option = ReadLine().ToLower().Trim()[0];
+            } while (option == 's');
+
+            WriteLine("\n¡Gracias por utilizar el programa!");
         }
     }
 }
