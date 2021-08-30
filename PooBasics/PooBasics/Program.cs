ce1992ec36526be72e855b17d34f9df40c0be749
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-08-30 12:12:35 -0500

    PooBasics project added

---
+++ PooBasics/PooBasics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace PooBasics
+{
+    struct Box
+    {
+        public int width;
+        public int length;
+        public int height;
+    }
+
+    class Program
+    {
+        static int CalculateVolume(Box box) =>
+            box.width * box.length * box.height;
+
+        static void Main(string[] args)
+        {
+            Box b1, b2, b3;
+            b1.width = 1; b1.length = 2; b1.height = 2;
+            b2.width = 3; b2.length = 5; b2.height = 3;
+            b3.width = 2; b3.length = 1; b3.height = 4;
+
+            Box[] boxes = { b1, b2, b3 };
+            foreach (Box box in boxes)
+            {
+                WriteLine($"Volume: {CalculateVolume(box)}");
+            }
+        }
+    }
+}
