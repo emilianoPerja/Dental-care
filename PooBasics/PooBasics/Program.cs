89cbeb841ad7f69ec825d05127d9506640ab62aa
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-09-02 11:21:26 -0500

    Update Program.cs

--- PooBasics/PooBasics/Program.cs
+++ PooBasics/PooBasics/Program.cs
@@ -8,29 +8,31 @@ using static System.Console;
 
 namespace PooBasics
 {
-    struct Box
+    class Box
     {
         public int width;
         public int length;
         public int height;
+
+        public static int CalculateVolume(Box box) =>
+            box.width * box.length * box.height;
     }
 
     class Program
     {
-        static int CalculateVolume(Box box) =>
-            box.width * box.length * box.height;
-
         static void Main(string[] args)
         {
-            Box b1, b2, b3;
+            Box b1 = new Box();
+            Box b2 = new Box();
+            Box b3 = new Box();
             b1.width = 1; b1.length = 2; b1.height = 2;
-            b2.width = 3; b2.length = 5; b2.height = 3;
-            b3.width = 2; b3.length = 1; b3.height = 4;
+            b2.width = 3; b2.length = 5; b2.height = 0;
+            b3.width = 2; b3.length = 1; b3.height = -4;
 
-            Box[] boxes = { b1, b2, b3 };
+            Box[] boxes = new Box[] { b1, b2, b3 };
             foreach (Box box in boxes)
             {
-                WriteLine($"Volume: {CalculateVolume(box)}");
+                WriteLine($"Volume: {Box.CalculateVolume(box)}");
             }
         }
     }
