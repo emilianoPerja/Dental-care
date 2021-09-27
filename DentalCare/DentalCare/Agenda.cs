78d437b89ae262f6dd2675b8add41a664f3fe2ce
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-09-27 16:28:56 -0500

    DentalCare project updated

--- DentalCare/DentalCare/Agenda.cs
+++ DentalCare/DentalCare/Agenda.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Util;
 
 namespace DentalCare
 {
     class Agenda
     {
+        private List<Day> days;
+        private List<Time> times;
+        private List<Schedule> schedules;
+        private List<Patient> patients;
+        private List<Appointment> appointments;
+
         public Agenda()
         {
-            // cargar información de archivos
+            // 1. Abrir archivo
+            // 2. Leer (for) cada línea del archivo => "0|Lunes"
+            // 3. ---- Separar datos por el caracter '|'
+            //         y forma arreglo de strings => ["0", "Lunes"]
+            // 4. Proporcionar función para convertir arreglo de strings
+            //    en clase deseada (Day, Time, etc.)
+            days = EasyFile<Day>.LoadDataFromFile("days.txt",
+                            t => new Day(Convert.ToInt32(t[0]), t[1]));
+
+            times = EasyFile<Time>.LoadDataFromFile("time.txt",
+                            t => new Time(Convert.ToInt32(t[0]), t[1]));
+
+            schedules = EasyFile<Schedule>.LoadDataFromFile("schedule.txt",
+                            t => new Schedule(Convert.ToInt32(t[0]),
+                                              Convert.ToInt32(t[1])));
+
+            patients = EasyFile<Patient>.LoadDataFromFile("patients.txt",
+                            t => new Patient(Convert.ToInt32(t[0]), t[1], t[2]));
+
+            appointments = EasyFile<Appointment>.LoadDataFromFile("appointments.txt",
+                            t => new Appointment(Convert.ToInt32(t[0]),
+                                                 Convert.ToInt32(t[1]),
+                                                 Convert.ToInt32(t[2])));
         }
     }
 }
--- DentalCare/DentalCare/Program.cs
+++ DentalCare/DentalCare/Program.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace DentalCare
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Agenda agenda = new Agenda();
 
+            int option = 0;
+            do
+            {
+                Clear();
+                WriteLine("*****************************************");
+                WriteLine("*              DENTAL CARE              *");
+                WriteLine("*            -Menú principal-           *");
+                WriteLine("*****************************************");
+                WriteLine("");
+                WriteLine("1. Consultas");
+                WriteLine("2. Citas");
+                WriteLine("0. Salir");
+
+                Write("\nElige una opción: ");
+                option = Convert.ToInt32(ReadLine());
+                
+                switch (option)
+                {
+                    case 1:
+                    case 2:
+                        WriteLine("\n¡Opción NO implementada!");
+                        ReadKey();
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        WriteLine("\n¡Opción NO válida!");
+                        ReadKey();
+                        break;
+                }
+            }
+            while (option != 0);
+
+            WriteLine("\n¡Gracias por usar el programa!");
         }
     }
 }
