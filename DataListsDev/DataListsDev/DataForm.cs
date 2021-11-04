511331ff83953acc5a8eb6da9e68acceeccad08f
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-11-04 12:25:17 -0600

    Update DataForm.cs

--- DataListsDev/DataListsDev/DataForm.cs
+++ DataListsDev/DataListsDev/DataForm.cs
@@ -44,13 +44,61 @@ namespace DataListsDev
             //cmbPatients.DisplayMember = "FullName";
 
             // #3 - DataSource - strings
-            List<string> patients = new List<string>();
-            patients.Add("Pedro");
-            patients.Add("Javier");
-            patients.Add("Erika");
-            patients.Add("Miguel");
-            patients.Add("Elizabeth");
+            //List<string> patients = new List<string>();
+            //patients.Add("Pedro");
+            //patients.Add("Javier");
+            //patients.Add("Erika");
+            //patients.Add("Miguel");
+            //patients.Add("Elizabeth");
 
+            //cmbPatients.DataSource = patients;
+
+            // #4 - DataSource - objects
+            //List<Client> patients = new List<Client>();
+            //patients.Add(new Client(689155, "Xavier", "Quintas"));
+            //patients.Add(new Client(917169, "Jaume", "Alguacil"));
+            //patients.Add(new Client(147222, "Jose", "Maldonado"));
+            //patients.Add(new Client(387368, "Rafael", "Caballero"));
+            //patients.Add(new Client(876855, "Maria", "Ramos"));
+            //patients.Add(new Client(736932, "Francisca", "Jurado"));
+            //patients.Add(new Client(502676, "Luis", "Ros"));
+            //patients.Add(new Client(186080, "Antonio", "Vargas"));
+            //patients.Add(new Client(832366, "Javier", "Esteban"));
+            //patients.Add(new Client(379061, "Marc", "Arroyo"));
+            //patients.Add(new Client(567213, "Jesus", "Bermudez"));
+            //patients.Add(new Client(183757, "Ruben", "Escribano"));
+            //patients.Add(new Client(676399, "Diego", "Cardona"));
+            //patients.Add(new Client(376270, "Angela", "Iglesias"));
+            //patients.Add(new Client(980040, "Francisco", "Montiel"));
+            //patients.Add(new Client(873028, "Silvia", "Huerta"));
+            //patients.Add(new Client(358176, "Ander", "Correa"));
+
+            //cmbPatients.DisplayMember = "FullName";
+            //cmbPatients.ValueMember = "Id";
+            //cmbPatients.DataSource = patients;
+
+            // #5 - DataSource - objects - binding
+            BindingList<Client> patients = new BindingList<Client>();
+            patients.Add(new Client(689155, "Xavier", "Quintas"));
+            patients.Add(new Client(917169, "Jaume", "Alguacil"));
+            patients.Add(new Client(147222, "Jose", "Maldonado"));
+            patients.Add(new Client(387368, "Rafael", "Caballero"));
+            patients.Add(new Client(876855, "Maria", "Ramos"));
+            patients.Add(new Client(736932, "Francisca", "Jurado"));
+            patients.Add(new Client(502676, "Luis", "Ros"));
+            patients.Add(new Client(186080, "Antonio", "Vargas"));
+            patients.Add(new Client(832366, "Javier", "Esteban"));
+            patients.Add(new Client(379061, "Marc", "Arroyo"));
+            patients.Add(new Client(567213, "Jesus", "Bermudez"));
+            patients.Add(new Client(183757, "Ruben", "Escribano"));
+            patients.Add(new Client(676399, "Diego", "Cardona"));
+            patients.Add(new Client(376270, "Angela", "Iglesias"));
+            patients.Add(new Client(980040, "Francisco", "Montiel"));
+            patients.Add(new Client(873028, "Silvia", "Huerta"));
+            patients.Add(new Client(358176, "Ander", "Correa"));
+            
+            cmbPatients.DisplayMember = "FullName";
+            cmbPatients.ValueMember = "Id";
             cmbPatients.DataSource = patients;
         }
 
@@ -70,10 +118,27 @@ namespace DataListsDev
             //btnRemove.Enabled = true;
 
             // #3 - DataSource - strings
-            txtIndex.Text = $"{cmbPatients.SelectedIndex}"; // OK
-            txtItem.Text = $"{cmbPatients.SelectedItem}";   // OK
-            txtValue.Text = $"{cmbPatients.SelectedValue}"; // OK
-            btnRemove.Enabled = true;
+            //txtIndex.Text = $"{cmbPatients.SelectedIndex}"; // OK
+            //txtItem.Text = $"{cmbPatients.SelectedItem}";   // OK
+            //txtValue.Text = $"{cmbPatients.SelectedValue}"; // OK
+            //btnRemove.Enabled = cmbPatients.SelectedIndex >= 0;
+
+            // #4 - DataSource - objects
+            if (cmbPatients.SelectedIndex >= 0)
+            {
+                txtIndex.Text = $"{cmbPatients.SelectedIndex}"; // OK
+                Client client = cmbPatients.SelectedItem as Client; // casting
+                txtItem.Text = $"{client.Id} - {client.FullName}";   // OK
+                txtValue.Text = $"{cmbPatients.SelectedValue}"; // OK
+            }
+            else
+            {
+                txtIndex.Text = "";
+                txtItem.Text = "";
+                txtValue.Text = "";
+            }
+
+            btnRemove.Enabled = cmbPatients.SelectedIndex >= 0;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -85,10 +150,23 @@ namespace DataListsDev
             //cmbPatients.Items.Add(new Client(623971, "Ramiro", "Jimenez"));
 
             // #3 - DataSource - strings
-            List<string> patients = cmbPatients.DataSource as List<string>;
-            patients.Add("Ramiro");
-            cmbPatients.DataSource = null;
-            cmbPatients.DataSource = patients;
+            //List<string> patients = cmbPatients.DataSource as List<string>;
+            //patients.Add("Ramiro");
+            //cmbPatients.DataSource = null;
+            //cmbPatients.DataSource = patients;
+
+            // #4 - DataSource - objects
+            //List<Client> patients = cmbPatients.DataSource as List<Client>;
+            //patients.Add(new Client(623971, "Ramiro", "Jimenez"));
+            //cmbPatients.DataSource = null;
+            //cmbPatients.DisplayMember = "FullName";
+            //cmbPatients.DataSource = patients;
+
+            // #5 - DataSource - objects - binding
+            BindingList<Client> patients = cmbPatients.DataSource as BindingList<Client>;
+            patients.Add(new Client(623971, "Ramiro", "Jimenez"));
+            patients.ResetBindings();
+            cmbPatients_SelectedIndexChanged(null, null); // call function()
         }
 
         private void btnRemove_Click(object sender, EventArgs e)
@@ -101,6 +179,35 @@ namespace DataListsDev
             //btnRemove.Enabled = false;
 
             // #3 - DataSource - strings
+            //List<string> patients = cmbPatients.DataSource as List<string>;
+            //patients.RemoveAt(cmbPatients.SelectedIndex);
+            //cmbPatients.DataSource = null;
+            //cmbPatients.DataSource = patients;
+
+            //if (cmbPatients.SelectedIndex == -1 && patients.Count > 0)
+            //{
+            //    cmbPatients.SelectedIndex = patients.Count - 1;
+            //}
+
+            // #4 - DataSource - objects
+            //List<Client> patients = cmbPatients.DataSource as List<Client>;
+            //patients.RemoveAt(cmbPatients.SelectedIndex);
+            //cmbPatients.DataSource = null;
+            //cmbPatients.DataSource = patients;
+
+            //if (cmbPatients.SelectedIndex == -1 && patients.Count > 0)
+            //{
+            //    cmbPatients.SelectedIndex = patients.Count - 1;
+            //}
+
+            //cmbPatients.DisplayMember = "FullName";
+
+            // #5 - DataSource - objects - binding
+            BindingList<Client> patients = cmbPatients.DataSource as BindingList<Client>;
+            patients.RemoveAt(cmbPatients.SelectedIndex);
+            patients.ResetBindings();
+            cmbPatients_SelectedIndexChanged(null, null); // call function()
+            //cmbPatients.SelectedIndex = patients.Count - 1;
         }
     }
 
